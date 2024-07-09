cd2547035325f6f92948b407dd4bdf55ca9e0237
Samwiseg0 <2241731+samwiseg0@users.noreply.github.com>
2024-07-09 09:44:12 -0400

    Backfill - Plex version 1.40.4.8679-424562606

--- plex-ffmpeg-source/PlexTranscoder/compat/cuda/dynlink_loader.h
+++ plex-ffmpeg-source/PlexTranscoder/compat/cuda/dynlink_loader.h
@@ -26,7 +26,7 @@
 #define FFNV_SYM_FUNC(lib, sym) dlsym((lib), (sym))
 #define FFNV_FREE_FUNC(lib) dlclose(lib)
 #define FFNV_LOG_FUNC(logctx, msg, ...) av_log(logctx, AV_LOG_ERROR, msg,  __VA_ARGS__)
-#define FFNV_DEBUG_LOG_FUNC(logctx, msg, ...) av_log(logctx, AV_LOG_DEBUG, msg,  __VA_ARGS__)
+#define FFNV_DEBUG_LOG_FUNC(logctx, msg, ...) av_log(logctx, AV_LOG_TRACE, msg,  __VA_ARGS__)
 
 #include <ffnvcodec/dynlink_loader.h>
 
